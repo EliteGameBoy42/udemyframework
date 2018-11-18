884ba5ab5e1f42cb46f479941ded0f5944145583
sislam <syfulislam42@hotmail.com>
2018-11-18 01:45:02 -0500

    this is a maven practice from udemy

---
+++ src/test/java/facebook/AppiumTest.java
@@ -0,0 +1,15 @@
+package facebook;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void NativeAppiumAndroid(){
+        System.out.println("NativeAppiumAndroid");
+
+    }
+    @Test
+    public void IOSapps(){
+        System.out.println("IOSapps");
+    }
+}
---
+++ src/test/java/facebook/SeleniumTest.java
@@ -0,0 +1,18 @@
+package facebook;
+
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+     @Test
+    public void BrowserAutomation(){
+         System.out.println("BrowserAtomation");
+
+    }
+    @Test
+    public void elementsUi(){
+        System.out.println("elementsUi");
+
+    }
+
+}
